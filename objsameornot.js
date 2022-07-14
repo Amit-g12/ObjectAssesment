d86d183925ea024a247d012a9c8307fb4a9b436c
Amit Kumar <amitkumargang123@gmail.com>
2022-07-14 17:32:07 +0530

    object

---
+++ objsameornot.js
@@ -0,0 +1,16 @@
+//Check whether the Objects are same or not.
+
+//Complete the function in the editor. In which you are given an object as a parameter. That object contains two variables name and id.
+
+
+
+function check(obj1,a,b) {
+    if (obj1.name ===a && obj1.id === b){
+      return "true";
+    }
+    else {
+      return "false";
+    }
+}
+  
+  
